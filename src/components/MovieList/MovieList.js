819383f7d584174a0af1b2fe0b8702d49d821cd6
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-04-26 17:42:45 +0200

    #2: Refactored icon to remove duplication of code

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -4,6 +4,21 @@ const MovieList = (props) => {
     const isFavourite = (movie) => {
         return !!props.favouriteMovies?.some(m => m.imdbID === movie.imdbID);
     }
+
+    const heartIcon = (color) => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg"
+                 width="1.5rem"
+                 height="1.5rem"
+                 fill={color}
+                 className="bi bi-heart-fill"
+                 viewBox="0 0 16 16"
+            >
+                <path fillRule="evenodd"
+                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
+            </svg>
+        )
+    }
     return (
         <>
             {props.movies?.map((movie, index) => (
@@ -18,28 +33,9 @@ const MovieList = (props) => {
                         <div className="float-right heart-icon"  onClick={() => props.handleFavouritesClick(movie)}>
                         {isFavourite(movie) ?
 
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                 width="1.5rem"
-                                 height="1.5rem"
-                                 fill="#ff5352"
-                                 className="bi bi-heart-fill"
-                                 viewBox="0 0 16 16"
-                            >
-                                <path fillRule="evenodd"
-                                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                            </svg>
+                            heartIcon("#ff5352")
                             :
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                 width="1.5rem"
-                                 height="1.5rem"
-                                 fill="white"
-                                 fillOpacity="0.5"
-                                 className="bi bi-heart-fill"
-                                 viewBox="0 0 16 16"
-                            >
-                                <path fillRule="evenodd"
-                                      d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"/>
-                            </svg>
+                            heartIcon("white")
                         }
                         </div>
                     </div>
