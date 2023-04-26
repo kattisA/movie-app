5c732286a6e1c1669a5fba82588e02f9e6517479
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-04-26 17:11:22 +0200

    #1: Fixed key warning

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -7,7 +7,7 @@ const MovieList = (props) => {
     return (
         <>
             {props.movies?.map((movie, index) => (
-                <div className='image-container d-flex justify-content-start m-3'>
+                <div className='image-container d-flex justify-content-start m-3' key={index}>
                     {movie.Poster !== "N/A" ?
                         <img src={movie.Poster} alt={"Poster of " + movie.Title}></img>
                         :
