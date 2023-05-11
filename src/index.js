4e21ecd02e4d22f7e8356c8579a439aa3f885c2b
Katarina Lejonlid <katarina.lejonlid@nbis.se>
2023-05-11 14:27:39 +0200

    #12: Added real generator favicon and added as component instead

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Favicon from "./components/Favicon/favicon";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Favicon />
     <App />
   </React.StrictMode>
 );
